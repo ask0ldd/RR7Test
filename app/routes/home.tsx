8f95cd907559cdefb3509035db18bee2c1ff5012
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-05-06 05:31:30 +0200

    *home* front / hero section v 1

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,15 +2,25 @@ import type { Route } from "./+types/home";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "ForginSquad" },
+    { name: "description", content: "Welcome to ForginSquad!" },
   ];
 }
 
 export default function Home() {
   return (
-    <div className="flex w-full h-[60px] border-b-1 border-[#eeeeee] justify-center items-center">
-      <span>Home</span>
+    <div className="flex flex-col w-full justify-center items-center font-poppins">
+      <div className="flex flex-col w-full max-w-[1280px] pt-[60px]">
+        <div className="flex flex-row w-fit px-[15px] h-[30px] text-[14px] text-[#004746] font-semibold border-1 border-[#F0F4F7] rounded-full gap-x-[10px] justify-center items-center self-center">
+          <span>A</span><span>POWERING THE NEXT BIG THING</span>
+        </div>
+        <h1 className="mt-[15px] text-[72px] text-[#004746] text-center font-medium leading-[110%] tracking-tighter self-center max-w-[1000px]">
+          From Idea To Success—Build, Launch, And Grow <span className="text-[#00BC72]">Faster</span>.
+        </h1>
+        <span className="mt-[25px] text-[#727272] font-sans text-center font-normal max-w-[640px] self-center">
+          We craft high-performing digital products that transform ideas into thriving businesses—built for growth, differentiation, and lasting impact.
+        </span>
+      </div>
     </div>
   );
 }
